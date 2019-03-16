301d7b9234758ea19d502d172ea25db60278376e
guisbonddev <wguisbond@gmail.com>
2019-03-16 16:38:24 -0400

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,41 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import Logo from "../components/image"
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `white`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 400,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            textDecoration: `none`,
+          }}
+        >
+          <Logo />
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
