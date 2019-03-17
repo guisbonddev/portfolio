cabd500bc6ef45a7f6bae252aaaf5b4ed890f8e0
guisbonddev <wguisbond@gmail.com>
2019-03-17 16:25:20 -0400

    NavBar complete, temporary BG layout with ReactStrap

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,11 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import Logo from "../components/image"
+
 import { TypographyStyle, GoogleFont } from 'react-typography'
 import typography from '../utils/typography'
+
+import Nav from 'react-bootstrap/Nav'
 
 const Header = ({ siteTitle }) => (
   <header
@@ -11,13 +14,28 @@ const Header = ({ siteTitle }) => (
       background: `transparent`,
     }}
   >
-    
     <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
+    <script src="https://unpkg.com/react/umd/react.production.js" crossorigin />
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
+    <script
+      src="https://unpkg.com/react-dom/umd/react-dom.production.js"
+      crossorigin
+    />
+
+    <script
+      src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+      crossorigin
+    />
+
+  <link
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+    integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+    crossorigin="anonymous"
+  />
+
+    <script>var Alert = ReactBootstrap.Alert;</script>
 
     <div
       style={{
@@ -25,6 +43,7 @@ const Header = ({ siteTitle }) => (
         maxWidth: 400,
       }}
     >
+
       <h3 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -36,6 +55,22 @@ const Header = ({ siteTitle }) => (
         </Link>
       </h3>
     </div>
+
+    <Nav style={{ paddingTop: `20px`}} className="justify-content-center" activeKey="/home">
+      <Nav.Item>
+        <Nav.Link style={{ textDecoration: `none`, color: `black`}} href="/home">About</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link style={{ textDecoration: `none`, color: `black`}} href="/home">Moments</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link style={{ textDecoration: `none`, color: `black`}} href="/home">Work</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link style={{ textDecoration: `none`, color: `black`}} href="/home">Experience</Nav.Link>
+      </Nav.Item>
+    </Nav>
+    
     <TypographyStyle typography={typography} />
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,31 @@ import Layout from "../components/layout"
 import SEO from "../components/seo" 
 import "./main.css"
 
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Will Guisbond" keywords={[`developer`, `portfolio`, `pilot`,`syracuse`]} />
     <center>
       <h1 class="big">Hi, I'm Will</h1>
-      <p class="subtitle">I'm a full stack developer based out of Syracuse, NY</p>
+      <p class="subtitle">I'm a full stack developer based out of Syracuse, NY</p><br></br>
     </center>
+
+  <Container>
+    <Row>
+      <Col><center><h1 class="big">About</h1></center></Col>
+      <Col><center><h1 class="big">Moments</h1></center></Col>
+    </Row>
+  </Container>
+  <Container>
+    <Row>
+      <Col><center><h1 class="big">Work</h1></center></Col>
+      <Col><center><h1 class="big">Exp.</h1></center></Col>
+    </Row>
+  </Container>
+    
     
   </Layout>
 )
